8ce8f890f61c407583403d0287bbb08b2f20dd47
janmmeier <Jan-Markus.Meier@tptlive.ee>
2021-10-29 22:27:38 +0300

    basic calc

---
+++ MainActivity.cs
@@ -0,0 +1,84 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace MobileApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView firstText;
+        TextView answerTextView;
+        EditText firstNumberEditText;
+        EditText secondNumberEditText;
+        Button addButton;
+        Button subButton;
+        Button multButton;
+        Button divButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            firstNumberEditText = FindViewById<EditText>(Resource.Id.editText1);
+            secondNumberEditText = FindViewById<EditText>(Resource.Id.editText2);
+            addButton = FindViewById<Button>(Resource.Id.addButton);
+            subButton = FindViewById<Button>(Resource.Id.subButton);
+            multButton = FindViewById<Button>(Resource.Id.multButton);
+            divButton = FindViewById<Button>(Resource.Id.divButton);
+            answerTextView = FindViewById<TextView>(Resource.Id.anwserTextView);
+            addButton.Click += AddButton_Click;
+            subButton.Click += SubButton_Click;
+            multButton.Click += MultButton_Click;
+            divButton.Click += DivButton_Click;
+        }
+
+        private void DivButton_Click(object sender, System.EventArgs e)
+        {
+            var firstNumber = double.Parse(firstNumberEditText.Text);
+            var secondNumber = double.Parse(secondNumberEditText.Text);
+            var answer = firstNumber / secondNumber;
+
+            answerTextView.Text = answer.ToString();
+        }
+
+        private void MultButton_Click(object sender, System.EventArgs e)
+        {
+            var firstNumber = double.Parse(firstNumberEditText.Text);
+            var secondNumber = double.Parse(secondNumberEditText.Text);
+            var answer = firstNumber * secondNumber;
+
+            answerTextView.Text = answer.ToString();
+        }
+
+        private void SubButton_Click(object sender, System.EventArgs e)
+        {
+            var firstNumber = double.Parse(firstNumberEditText.Text);
+            var secondNumber = double.Parse(secondNumberEditText.Text);
+            var answer = firstNumber - secondNumber;
+
+            answerTextView.Text = answer.ToString();
+        }
+
+        private void AddButton_Click(object sender, System.EventArgs e)
+        {
+            var firstNumber = double.Parse(firstNumberEditText.Text);
+            var secondNumber = double.Parse(secondNumberEditText.Text);
+            var answer = firstNumber + secondNumber;
+
+            answerTextView.Text = answer.ToString();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
